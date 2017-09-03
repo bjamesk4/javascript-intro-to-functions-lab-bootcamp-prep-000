aae4ab33b03c1f9e779167b2eccd867943619765
Beverly kokos <bjamesk4@gmail.com>
2017-09-03 05:40:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,4 @@ function sayHiToGrandma(string) {
   else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
-  else (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
-
 }
