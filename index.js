cb3035b050bd69a11acc8d064b7447fd33ff7db5
Beverly kokos <bjamesk4@gmail.com>
2017-09-03 05:30:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase === string) {
-    return "YES INDEED!"
-}
+  if (string.toLowerCase === string) {
+    return "I can't hear you!"
+    
+  } else {
+    
+  }
